5d3e0052652fcc6c7733435fad6a999ebb01f1c1
ThanhHai <thanhhaitran1203@gmail.com>
2024-03-18 16:46:47 +0700

    first commit

---
+++ src/test/java/com/example/customersjwtajax/CustomersJwtAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customersjwtajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersJwtAjaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
